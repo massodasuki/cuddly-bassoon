ce37a423d706df7ef2256cf0b29a10e444543d28
massodasuki <massodasuki@gmail.com>
2025-10-11 22:18:27 +0800

    revamp

--- elesen-api-v1/src/fishing-activity/dto/update-fishing-activity.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/swagger';
-import { CreateFishingActivityDto } from './create-fishing-activity.dto';
-
-export class UpdateFishingActivityDto extends PartialType(CreateFishingActivityDto) {}
--- elesen-api-v1/src/pengkalan/dto/update-pengkalan.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/swagger';
-import { CreatePengkalanDto } from './create-pengkalan.dto';
-
-export class UpdatePengkalanDto extends PartialType(CreatePengkalanDto) {}
