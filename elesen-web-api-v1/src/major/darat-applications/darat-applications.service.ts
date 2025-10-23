de9de7e676a1dc40ff5e9ab29df27cfb6cb5e49e
massodasuki <massodasuki@gmail.com>
2025-10-23 18:19:52 +0800

    add darat-applications-minimal with id

--- elesen-web-api-v1/src/major/darat-applications/darat-applications.service.ts
+++ elesen-web-api-v1/src/major/darat-applications/darat-applications.service.ts
@@ -27,6 +27,7 @@ export class DaratApplicationsService {
     };
 
     const transformedData = data.map(item => ({
+      id : item.id,
       noVesel: item.daratVesselInspection?.daratVessel?.registration_number || item.daratVesselInspection?.vessel_registration_number || '',
       tarikhPemeriksaan: item.inspection_date ? formatDate(item.inspection_date) : '',
       zonOperasi: item.daratVesselInspection?.inspection_location || '',
