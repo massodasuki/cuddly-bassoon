d40f0b5420cca1de85bbe29f03ee886071626074
massodasuki <massodasuki@gmail.com>
2025-11-17 03:24:18 +0800

    add module

--- elesen-web-api-v1/src/major/darat-applications/darat-applications.module.ts
+++ elesen-web-api-v1/src/major/darat-applications/darat-applications.module.ts
@@ -3,6 +3,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { DaratApplicationsController } from './darat-applications.controller';
 import { DaratApplicationsService } from './darat-applications.service';
 import { DaratApplicationEntity } from '../darat-applications/darat-applications.entity';
+import { DaratVesselEntity } from '../darat-vessels/darat-vessels.entity';
 import { DaratVesselInspectionEntity } from '../darat-vessel-inspections/darat-vessel-inspections.entity';
 import { CodeMaster } from '../code-masters/code-masters.entity';
 
@@ -10,6 +11,7 @@ import { CodeMaster } from '../code-masters/code-masters.entity';
   imports: [
     TypeOrmModule.forFeature([
       DaratApplicationEntity,
+      DaratVesselEntity,
       DaratVesselInspectionEntity,
       CodeMaster,
     ]),
